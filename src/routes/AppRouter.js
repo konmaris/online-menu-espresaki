4b462ed6d18d01a727cf8ab71dd3eab26bdc6d45
konmaris <mariskon02@gmail.com>
2024-07-31 18:30:40 +0300

    add

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Menu from "../components/Menu";
 
 function AppRouter() {
